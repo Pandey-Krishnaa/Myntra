4b842cb16711b860245376a505a67b1e5b452e13
krishna-ship-it <krish36pandey@gmail.com>
2023-07-05 14:27:52 +0530

    initial commits

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -48,17 +48,17 @@ export const createProduct = catchAsync(async (req, res, next) => {
     forWhom,
     brand,
   });
-  // const cloudinaryPromises = images.map((image) =>
-  //   cloudinary.v2.uploader.upload(image.tempFilePath)
-  // );
-  // let cloudImg;
-  // try {
-  //   cloudImg = await Promise.all(cloudinaryPromises);
-  //   console.log(cloudImg);
-  // } catch (err) {
-  //   console.log("error---->", err.message);
-  // }
-  // console.log("uploaded");
+  const cloudinaryPromises = images.map((image) =>
+    cloudinary.v2.uploader.upload(image.tempFilePath)
+  );
+  let cloudImg;
+  try {
+    cloudImg = await Promise.all(cloudinaryPromises);
+    console.log(cloudImg);
+  } catch (err) {
+    console.log("error---->", err.message);
+  }
+  console.log("uploaded");
   // const imagesArr = [];
   // cloudImg.forEach((img) => {
   //   imagesArr.push({ url: img.secure_url, public_id: img.public_id });
