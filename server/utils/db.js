109b63ecb655e6401176b2dbe6d9ac1bc4077a8b
Krishna Pandey <73630207+krishna-ship-it@users.noreply.github.com>
2024-02-25 09:28:05 +0530

    Update db.js

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,19 +1,32 @@
 import mongoose from "mongoose";
 
-const connectDb = () => {
+const connectDb = async () => {
   const str = process.env.DB_URL.replace("<password>", process.env.DB_PASS);
-  mongoose
-    .connect(str, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      console.log("connected with db");
-    })
-    .catch((err) => {
-      console.log(err);
-      console.log("failed to connect with db");
-    });
+  try{     
+      await mongoose
+        .connect(str, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        })
+  }
+  catch(err)
+  {
+    consolee.log(err.message);
+    throw err;
+  }
+  // const str = process.env.DB_URL.replace("<password>", process.env.DB_PASS);
+  // mongoose
+  //   .connect(str, {
+  //     useNewUrlParser: true,
+  //     useUnifiedTopology: true,
+  //   })
+  //   .then(() => {
+  //     console.log("connected with db");
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //     console.log("failed to connect with db");
+  //   });
 };
 
 export default connectDb;
